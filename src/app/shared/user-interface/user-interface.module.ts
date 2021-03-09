fd6379b67b974e0e05a73697d736087ab393837d
Jean-Luc Aubert <jean-luc.aubert@aelion.fr>
2021-03-09 13:13:08 +0100

    Add some Materials to components

--- src/app/shared/user-interface/user-interface.module.ts
+++ src/app/shared/user-interface/user-interface.module.ts
@@ -3,17 +3,27 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+const materials: any[] = [
+  MatToolbarModule,
+  MatButtonToggleModule
+];
+
 @NgModule({
   declarations: [
     HeaderComponent,
     FooterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ...materials
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ...materials
   ]
 })
 export class UserInterfaceModule { }
